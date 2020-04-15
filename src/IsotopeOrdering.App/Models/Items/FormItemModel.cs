9ad95109cba4022650cbf4cedd626bcf55e8586b
Dylan Farrell <dylanmfarrell@gmail.com>
2020-04-15 16:22:56 -0500

    More controllers added and unit tests.

---
+++ src/IsotopeOrdering.App/Models/Items/FormItemModel.cs
@@ -0,0 +1,12 @@
+﻿using IsotopeOrdering.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsotopeOrdering.App.Models.Items {
+    public class FormItemModel {
+        public int Id { get; set; }
+        public FormType Type { get; set; }
+
+    }
+}

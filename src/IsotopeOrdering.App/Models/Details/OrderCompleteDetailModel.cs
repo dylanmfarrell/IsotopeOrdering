360263b868d8a091969033bc5a61330082c1681b
Dylan Farrell <dylanmfarrell@gmail.com>
2020-08-07 09:57:03 -0500

    Fix default values not being posted on initiation form completion. Refactor item details into partial

--- src/IsotopeOrdering.App/Models/Details/OrderCompleteDetailModel.cs
+++ src/IsotopeOrdering.App/Models/Details/OrderCompleteDetailModel.cs
@@ -5,6 +5,6 @@ namespace IsotopeOrdering.App.Models.Details {
     public class OrderCompleteDetailModel {
         public OrderReviewDetailModel OrderReview { get; set; } = new OrderReviewDetailModel();
         public List<ShipmentDetailModel> Shipments { get; set; } = new List<ShipmentDetailModel>();
-        public decimal Total => decimal.Round(OrderReview.Order.Cart.Sum(x => x.Price) + Shipments.Sum(x => x.ShippingCharge),2)
+        public decimal Total => decimal.Round(OrderReview.Order.Cart.Sum(x => x.Price) + Shipments.Sum(x => x.ShippingCharge), 2);
     }
 }
